5aec3668c294736d4be081082f0378dfa0629c37
anujkkarmakar <anujkarmakar999@gmail.com>
2023-04-30 12:47:40 +0530

    Updated ItemOrderScreen

--- app/src/main/java/com/example/lunchtray/ui/OrderViewModel.kt
+++ app/src/main/java/com/example/lunchtray/ui/OrderViewModel.kt
@@ -3,6 +3,7 @@ package com.example.lunchtray.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.lunchtray.data.OrderUiState
+import com.example.lunchtray.model.MenuItem
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -16,5 +17,29 @@ class OrderViewModel : ViewModel() {
      */
     fun resetOrder() {
         _uiState.value = OrderUiState()
+    }
+
+    fun setEntreeItem(entreeItem: MenuItem.EntreeItem) {
+        _uiState.update {
+            it.copy(
+                entreeItem = entreeItem
+            )
+        }
+    }
+
+    fun setSideDishItem(sideDishItem: MenuItem.SideDishItem) {
+        _uiState.update {
+            it.copy(
+                sideDishItem = sideDishItem
+            )
+        }
+    }
+
+    fun setAccompanimentItem(accompanimentItem: MenuItem.AccompanimentItem) {
+        _uiState.update {
+            it.copy(
+                accompanimentItem = accompanimentItem
+            )
+        }
     }
 }
