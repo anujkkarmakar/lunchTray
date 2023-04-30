6603333b842462dcee577d74a63b3a4f4168d03c
anujkkarmakar <anujkarmakar999@gmail.com>
2023-04-30 09:29:38 +0530

    Added the UI Model for the View Model

--- app/src/main/java/com/example/lunchtray/data/OrderUiState.kt
+++ app/src/main/java/com/example/lunchtray/data/OrderUiState.kt
@@ -1,5 +1,10 @@
 package com.example.lunchtray.data
 
+import com.example.lunchtray.model.MenuItem
+
 data class OrderUiState(
-    
+    val entreeItem: MenuItem.EntreeItem? = null,
+    val sideDishItem: MenuItem.SideDishItem? = null,
+    val accompanimentItem: MenuItem.AccompanimentItem? = null,
+    val subTotal: Double = 0.0
 )
